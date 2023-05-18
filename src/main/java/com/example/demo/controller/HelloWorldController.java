5a97baa586acd21ea8fa5a0d9a2a729d681eaa15
adrixnac <adrianutm@gmail.com>
2023-05-18 12:26:28 -0600

    Update HelloWorldController.java

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hola mundo!!!!!!!!! V3";
+        return "Hola mundo!!!!!!!!! V5 Dev";
     }
 
     @GetMapping("/echo")
